08d5d630168d86f912e74be94e74e297b5594d2d
Farzad Azizi Zade <84402243+FarzadAziziZade@users.noreply.github.com>
2021-10-06 20:27:13 +0330

    Add files via upload

--- MLtest/TestSVR.py
+++ MLtest/TestSVR.py
@@ -11,13 +11,17 @@ header_list = ["f1", "f2", "f3", "f4", "o5", "o6", "o7"]
 data = pd.read_csv('{file_name}.csv'.format(file_name = csv_file_name), names=header_list).replace('?', np.NaN).dropna()
 X = data.get(["f1", "f2", "f3", "f4"])
 y = data.get(["o7"])
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+y = scaler.fit_transform(y)
 #-------------------------------------------------------
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle = True)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, shuffle = True)
 #-------------------------------------------------------
 rng = np.random.RandomState(0)
-regr = make_pipeline(StandardScaler(), SVR(kernel='poly', degree=20,C=1, epsilon=0.1))
+regr = make_pipeline(StandardScaler(), SVR(kernel='poly', degree=21,C=1, epsilon=0.1))
 #regr = make_pipeline(StandardScaler(), SVR(kernel='sigmoid',C=1, epsilon=0.1))
+#regr = make_pipeline(StandardScaler(), SVR(C=1, epsilon=0.1))
 #-------------------------------------------------------
 regr.fit(X, y)
 #-------------------------------------------------------
